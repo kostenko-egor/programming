06318975fc3e55771aa46a2c81afeab7eb5f0352
Студенческая <@rbiu.local>
2025-04-15 12:16:44 +0500

    задачи на цикл

---
+++ js/text2.js
@@ -0,0 +1,73 @@
+// 4.10
+// let count = x, y, a, b, max;
+//     x = parseInt(prompt("Введите радиус круга "))
+//     y = parseInt(prompt("Введите сторону квадрата "))
+//     a = x * 3,14
+//     b = y ** 2
+//     if (a > b)
+//     {
+//         a = (max +"круга") 
+//     }
+//     else 
+//     {
+//         b = (max +"квадрата")
+//     }
+//     alert("наибольшая площадь у " + max)
+
+//4.55
+// let count = x, y, a, b, c, d ;
+// x = parseInt(prompt("Введите двухзначное число"))
+// y = parseInt(prompt("Введите число"))
+// a = x % 10;
+// b = x % 100;
+// c = x % 10;
+// d = x % 100;
+// if ((a = 3) && (b = 3))
+// {
+//     alert("Входит")
+// }
+// else
+// {
+//     alert("Выходит")
+// }
+// if ((c = y) && (d = y))
+// {
+//     alert("Входит")
+// }
+// else
+// {
+//     alert("Выходит")
+// }
+
+//5.30
+// let count = a, b, c, d, e, f, x, y,i;
+// a = Math.pow(20,3)+Math.pow(21,3)+Math.pow(22,3)+Math.pow(23,3)+Math.pow(24,3)+Math.pow(25,3)+Math.pow(26,3)+Math.pow(27,3)+Math.pow(28,3)+Math.pow(29,3)+Math.pow(30,3)+Math.pow(31,3)+Math.pow(32,3)+Math.pow(33,3)+Math.pow(34,3)+Math.pow(35,3)+Math.pow(36,3)+Math.pow(37,3)+Math.pow(38,3)+Math.pow(39,3)+Math.pow(40,3)
+// alert(a)
+// x = parseInt(prompt("Введите двухзначное число"))
+// while(x <= 50)
+// {
+//     console.log(x);
+//     Math.pow(x,2)+b
+//     x++;
+// }
+// alert(b)
+// y = parseInt(prompt("Введите двухзначное число"))
+// while(i <= y)
+//     {
+//         console.log(i);
+//         Math.pow(i,2)+c
+//         i++;
+//     }
+// alert(c)
+// z = parseInt(prompt("Введите двухзначное число"))
+// while(e <= f)
+//     {
+//         console.log(e);
+//         Math.pow(e,2)+d
+//         e++;
+//     }
+// alert(d)
+
+//5.49
+
+
